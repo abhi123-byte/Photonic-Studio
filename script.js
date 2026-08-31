ad6e63f5a07b69d74c240b8388d25e70a35ca2e7
abhi123-byte <justf9695@gmail.com>
2026-09-01 00:51:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,14 +25,10 @@ document.querySelectorAll("section").forEach(section => {
     observer.observe(el);
   });
 });function toggleService(galleryId, element) {
+
     const gallery = document.getElementById(galleryId);
-    const plus = element.querySelector("b");
 
     gallery.classList.toggle("active");
 
-    if (gallery.classList.contains("active")) {
-        plus.textContent = "−";
-    } else {
-        plus.textContent = "+";
-    }
+    element.classList.toggle("open");
 }
